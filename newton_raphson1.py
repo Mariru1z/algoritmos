263a96bc1ce103d299f590742410aff4b45f6121
Mariru1z <maria.ruizsa@anahuac.mx>
2024-03-22 12:45:44 -0600

    v-0.

--- newton_raphson1.py
+++ newton_raphson1.py
@@ -1,5 +1,5 @@
 #crear un códifo que resuelva 2x^3+x^2 =1 por newton raphson en python
-#como algoritmo matematico  2x^3+x^2 =1 con metodo de newton raphson?
+#como algoritmo matematico  2x^3+x^2 =1 con metodo de newton raphson
 def func(x):
     return 2*x**3 + x**2 - 1
 
